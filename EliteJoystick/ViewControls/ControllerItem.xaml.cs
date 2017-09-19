ce0798e20ee7d3b0f6b233ddc30340573835ae09
Bob Helander <bob@helanderco.us>
2017-09-18 22:50:52 -0600

    Update UI

--- EliteJoystick/ViewControls/ControllerItem.xaml.cs
+++ EliteJoystick/ViewControls/ControllerItem.xaml.cs
@@ -24,5 +24,15 @@ namespace EliteJoystick.ViewControls
         {
             InitializeComponent();
         }
+
+        private void ProgressBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            var progressBar = sender as ProgressBar;
+            if (progressBar == null) return;
+
+            var animation = progressBar.Template.FindName("Animation", progressBar) as FrameworkElement;
+            if (animation != null)
+                animation.Visibility = Visibility.Collapsed;
+        }
     }
 }
