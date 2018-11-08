2e2e711f1792c6f9fcd88917904ed5fc8c35f45b
Bob Helander <bob@helanderco.us>
2018-11-07 19:49:50 -0700

    Testing lights

--- vJoyMapping/Thrustmaster/Warthog/Throttle/Controller.cs
+++ vJoyMapping/Thrustmaster/Warthog/Throttle/Controller.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using EliteJoystick.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Usb.GameControllers.Thrustmaster.Warthog.Throttle.Models;
 using vJoyMapping.Thrustmaster.Warthog.Throttle.Mapping;
 
 namespace vJoyMapping.Thrustmaster.Warthog.Throttle
@@ -21,32 +23,49 @@ namespace vJoyMapping.Thrustmaster.Warthog.Throttle
             joystick.Initialize();
         }
 
-        public void MapControls(Usb.GameControllers.Thrustmaster.Warthog.Throttle.Joystick ffb2)
+        public void MapControls(Usb.GameControllers.Thrustmaster.Warthog.Throttle.Joystick warthog)
         {
             // Add in the mappings
             Disposables = new List<IDisposable> {
-                ffb2.Subscribe(x => TmThrottle75Command.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleButtonStateHandler.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleCameraCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleClearMessages.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleCycleCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleHardpointsCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleHat.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleLandedStateHandler.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleLandingGearCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleLightsCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleSecondaryFireCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleSilentCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleSliderJoystick.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleStateModifier.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleVoiceCommandHandler.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleXYJoystick.Process(x, this), ex => log.Error($"Exception : {ex}")),
-                ffb2.Subscribe(x => TmThrottleZJoystick.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottle75Command.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleButtonStateHandler.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleCameraCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleClearMessages.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleCycleCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleHardpointsCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleHat.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleLandedStateHandler.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleLandingGearCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleLightsCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleSecondaryFireCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleSilentCommand.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleSliderJoystick.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleStateModifier.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleVoiceCommandHandler.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleXYJoystick.Process(x, this), ex => log.Error($"Exception : {ex}")),
+                warthog.Subscribe(x => TmThrottleZJoystick.Process(x, this), ex => log.Error($"Exception : {ex}")),
             };
         }
 
-        public void MapLights(Usb.GameControllers.Thrustmaster.Warthog.Throttle.Joystick ffb2)
+        public void MapLights(Usb.GameControllers.Thrustmaster.Warthog.Throttle.Joystick warthog)
         {
+            // Turn lights on and off
+            SharedState.ModeChanged.Subscribe(x => {
+                warthog.Lights = (byte)Light.LEDBacklight;
+                warthog.LightIntensity = (byte)GetIntensity(x);
+                });
+        }
+
+        private Intensity GetIntensity(EliteSharedState.Mode mode)
+        {
+            if (mode == EliteSharedState.Mode.Fighting)
+                return Intensity.EXTRA_HIGH;
+            if (mode == EliteSharedState.Mode.Travel)
+                return Intensity.MED;
+            if (mode == EliteSharedState.Mode.Mining)
+                return Intensity.EXTRA_LOW;
+
+            return Intensity.OFF;
         }
 
         public void Dispose()
